d87cf29353ef40cf5237ac0366f836bd70d07bb7
Tricky <tricky@Trickys-MacBook-Air.local>
2026-05-10 21:30:47 +0200

    Change environ

--- ad/adapters/utils.py
+++ ad/adapters/utils.py
@@ -7,7 +7,7 @@ BASE_DIR = (
 
 
 def get_config() -> configparser.ConfigParser:
-    config_file = BASE_DIR.joinpath('environment.ini')
+    config_file = BASE_DIR.joinpath('.env')
     config = configparser.ConfigParser()
     with open(config_file) as raw_config_file:
         config.read_file(raw_config_file)
